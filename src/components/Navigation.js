12a9b7148312031723daf5dfb459017e587e5f2e
TsikaBullRat <tsika1996190514@gmail.com>
2021-07-06 19:04:24 +0200

    running through changes

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,19 +1,17 @@
 
 export const Navigation = () => {
     return (
-        <nav className="row navbar navbar-expand-lg navbar-light">
-            <a href="#banner" className="navbar-brand">
-                <h1>Logo</h1>
-            </a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <nav className="navbar navbar-expand-lg navbar-light">
+            <a href="#banner" className="navbar-brand">Logo</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#Content" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
-            <div className="collapse navbar-collapse flex-row-reverse" id="navbarContent">
+            <div className="collapse navbar-collapse justify-content-end" id="Content">
                 <div className="navbar-nav">
-                    <a href="#banner" className="nav-item  active">Home</a>
-                    <a href="#aboutus" className="nav-item">About Us</a>
-                    <a href="#ourimapcts" className="nav-item">Our Impacts</a>
-                    <a href="#footer" className="nav-item">Conatacts</a>
+                    <div className=""><a href="#banner" className="nav-item active">Home</a></div>
+                    <div ><a href="#banner" className="nav-item">About Us</a></div>
+                    <div><a href="#banner"  className="nav-item">Our Impacts</a></div>
+                    <div ><a href="#banner" className="nav-item">Conatacts</a></div>
                 </div>
             </div>
         </nav>
